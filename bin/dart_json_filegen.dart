a195317bdad05c4e8648d49d32f3081ef2e30834
Khaldoun Badreah <102937196+khaldounB@users.noreply.github.com>
2024-08-30 13:30:28 +0300

    Update dart_json_filegen.dart

--- bin/dart_json_filegen.dart
+++ bin/dart_json_filegen.dart
@@ -6,22 +6,24 @@ import 'package:dart_json_filegen/dart_json_filegen.dart';
 /// and invoking the file generation logic.
 void main(List<String> arguments) async {
   // Check if the user provided the required arguments (path, file name, class name, folder, and JSON)
-  if (arguments.length < 5) {
+  if (arguments.length < 6) {
     print(
-        'Usage: dart create_file.dart <path> <file_name> <class_name> <folder> <json>');
+        'Usage: dart create_file.dart <app> <path> <file_name> <class_name> <folder> <json>');
     return;
   }
 
   // Extract the arguments
-  final pathSegment = arguments[0];
-  final fileName = arguments[1];
-  final className = arguments[2];
-  final folder = arguments[3];
-  final jsonString = arguments[4];
+  final app = arguments[0];
+  final pathSegment = arguments[1];
+  final fileName = arguments[2];
+  final className = arguments[3];
+  final folder = arguments[4];
+  final jsonString = arguments[5];
 
   // Instantiate and use the FileGenerator class
   final generator = FileGenerator();
   await generator.createFile(
+    app: app,
     pathSegment: pathSegment,
     fileName: fileName,
     className: className,
